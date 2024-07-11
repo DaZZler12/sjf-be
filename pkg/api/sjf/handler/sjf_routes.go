887ee9ebf50fee9eb8d13eaa8d152d063c1eb570
DaZZler12 <abhirupbhattacharya8@gmail.com>
2024-07-12 00:23:11 +0530

    websocket implemetaiton to get real time updates

--- pkg/api/sjf/handler/sjf_routes.go
+++ pkg/api/sjf/handler/sjf_routes.go
@@ -20,6 +20,8 @@ func SjfInit(route *gin.RouterGroup, logger *zap.Logger) {
 		{
 			version1.GET("", sjfHandler.List)
 			version1.GET("/:id", sjfHandler.Get)
+			// version1.GET("/ws", sjfHandler.GetWebSocketDataForJobIDs) // TODO: fetch status using websocket for multiple job IDs
+			version1.GET("/ws/:id", sjfHandler.GetJobStatusUsingWebSocket)
 			version1.POST("", sjfHandler.Create)
 			// version1.PUT("/update", sjfHandler.Update)
 			version1.DELETE("/:id", sjfHandler.Delete)
